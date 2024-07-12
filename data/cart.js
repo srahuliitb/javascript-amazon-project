903fde30c4153f8901f97700f1138dd57a45981d
srahuliitb <srahul.iitb@gmail.com>
2024-07-12 15:27:17 +0100

    Create delivery options and generate html for each cart item and display them on the checkout.html page

--- data/cart.js
+++ data/cart.js
@@ -3,15 +3,38 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if (!cart) {
   cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId: '1'
   }, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1
+    quantity: 1,
+    deliveryOptionId: '2'
   }];
 }
 
 export function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function calculateCartQuantity() {
+  let cartQuantity = 0;
+    
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+  return cartQuantity;
+}
+
+export function updateQuantity(productId, newQuantity) {
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId == cartItem.productId) {
+      matchingItem = cartItem;
+    }
+  });
+  matchingItem.quantity = newQuantity;
+  saveToStorage(); 
 }
 
 export function addToCart(productId, quantity) {
@@ -27,7 +50,8 @@ export function addToCart(productId, quantity) {
   } else {
     cart.push({
       productId,
-      quantity
+      quantity,
+      deliveryOptionId: '1'
     });
   }
   saveToStorage();
--- scripts/utils/money.js
+++ scripts/utils/money.js
@@ -1,3 +1,6 @@
 export function formatCurrency(priceCents) {
   return (priceCents / 100).toFixed(2);
 }
+
+// This is called 'Default Export'. Each file can have ONLY 1 default export. 
+export default formatCurrency;
