0a0be957e99764c96903a2dd765b378d43334aa5
srahuliitb <srahul.iitb@gmail.com>
2024-07-16 15:30:45 +0100

    Create a class called Cart to allow the creation of new instance of Cart whenever needed

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,8 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js'; // This is def
 import isSatSun from "./exercises/15f.js"; // We can rename a default export wehen we import.
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
 
-import '../data/cart-oop.js';
+// import '../data/cart-oop.js';
+import '../data/cart-class.js';
 
 renderCheckoutHeader();
 renderOrderSummary();
