2a671cece61897b2521a7004ca9cf88a35571939
srahuliitb <srahul.iitb@gmail.com>
2024-07-18 13:55:15 +0100

    Load products from backend server in beforeAll hook

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -1,10 +1,17 @@
 import { renderOrderSummary } from "../../scripts/checkout/orderSummary.js";
 import { loadCartFromStorage, cart } from "../../data/cart.js";
+import { loadProductsFromBackend } from "../../data/products.js";
 
 describe('test suit: renderOrderSummary', () => {
   const productId1 = "e43638ce-6aa0-4b85-b27f-e1d07eb678c6";
   const productId2 = "15b6fc6f-327a-4ec4-896f-486349e85a3d";
   
+  beforeAll((done) => {
+    loadProductsFromBackend(() => {
+      done(); // Done function waits for the response to be received first before executing the subsequent code.
+    });
+  });
+
   beforeEach(() => {
     spyOn(localStorage, 'setItem');
     document.querySelector('.js-test-container').innerHTML = `
